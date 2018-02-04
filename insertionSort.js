99d21e5522f38d0ab98b0297b6ac63d4439e9ace
Patrick Shannon <patrickshannon05021993@gmail.com>
2018-02-04 00:43:30 -0500

    Adds the insertions part of the insertion sort algorithm.

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,44 @@
 ////////////////////////////////////////////////////////////
 //  Sort an array by inserting them into a subarray in order 
 ////////////////////////////////////////////////////////////
+	
+function insert(value, position, array){
+	let tempArray = array.slice(0)
+	for (let i = array.length-1; i >= 0; i--){
+		if (i != position){
+			tempArray[i+1] = tempArray[i]
+		}
+		else{
+			tempArray[i+1] = tempArray[i]
+			tempArray[i] = value
+			break
+		}
+	}
+	console.log(tempArray)
+	return tempArray
+}
 
-
-
-let testArray = [1,7,4,8,9,0,3]
-
-function insert(){}
 
 //// Tests!!!
+let testArray = [1,7,4,8,9,0,3]
+let newArray = []
+function resetTestArrays(){
+	newArray = testArray.slice(0)
+}
 // insert
-console.assert(insert(6,1,testArray)===[1,6,7,4,8,9,0,3],"6 doesn't correctly get inserted into index 1 of 'testArray'${testArray}")
-console.log("6 correctly inserted into 'testArray'!")
-console.assert(insert(9,4,testArray)===[1,7,4,8,9,9,0,3],"9 doesn't correctly get inserted into index 4 of 'testArray'${testArray}")
+newArray = insert(6,1,testArray)
+console.assert(String(newArray) === String([1,6,7,4,8,9,0,3]),`6 doesn't correctly get inserted into index 1 of 'testArray' [${newArray}]`)
+console.log(`6 correctly inserted into 'testArray'!`)
+resetTestArrays()
+newArray = insert(2,6,testArray)
+console.assert(String(newArray) === String([1,7,4,8,9,0,2,300]),`2 doesn't correctly get inserted into index 6 of 'testArray' [${newArray}]`)
+console.log(`2 correctly inserted into 'testArray'!`)
+resetTestArrays()
+newArray = insert(9,7,testArray)
+console.assert(String(newArray) === String([1,7,4,8,9,0,3,9]),`9 doesn't correctly get inserted into index 7 of 'testArray' [${newArray}]`)
+console.log(`9 correctly inserted into 'testArray'!`)
+resetTestArrays()
+console.log('All insert tests complete')
+
+// sort
+
