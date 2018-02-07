8d6e2a6772a96a8a66d2c27e19e5a338809ff971
Patrick Shannon <patrickshannon05021993@gmail.com>
2018-02-06 23:25:32 -0500

    adds a completion log to the TestSeries' run method.

--- function-tester.js
+++ function-tester.js
@@ -3,24 +3,34 @@ function TestSeries() {
 	this.results = []
 }
 
-TestSeries.prototype.addTest = function(operation, assertion, ...inputs){
+TestSeries.prototype.addTest = function(operation, assertion, assertionDescription, ...inputs){
 	this.tests.push({
 		operation: operation,
 		assertion: assertion,
+		assertionDescription: assertionDescription,
 		inputs: inputs
 	})
 }
 
 TestSeries.prototype.run = function(){
+	let resultString = "=====[ BEGIN TESTS ]===== \n \n"
+	let passes = 0
 	for (let i = 0; i < this.tests.length; i++){
 		console.log('ding')
 		if(this.tests[i].operation(...this.tests[i].inputs)===this.tests[i].assertion){
 			this.results[i] = true
+			resultString += `..PASS..: ${this.tests[i].assertionDescription} \n`
+			++passes
 		}
 		else{
 			this.results[i] = false
+			resultString += `!!FAIL!!: ${this.tests[i].assertionDescription} \n`
 		}
 	}
+	resultString += "\n=====[   RESULTS   ]===== \n \n"
+	let percent = (passes/this.tests.length)*100
+	resultString += `${percent}% of tests passed. \n \n=====[  END TESTS  ]=====`
+	return resultString
 }
 ////TESTS
 //actually work
@@ -29,7 +39,7 @@ function sum(a,b=a){
 	return a+b
 }
 let meta = new TestSeries()
-meta.addTest(sum, 3, 1, 2)
-console.log(meta.tests)
-meta.run()
-console.log(meta.results)
+meta.addTest(sum, 3,"Adds 1 and 2 to get 3.", 1, 2)
+meta.addTest(sum, 9,"Adds two numbers to get 9.", 8,100)
+//console.log(meta.tests)
+console.log(meta.run())
