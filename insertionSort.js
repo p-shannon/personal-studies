dc648cda7c5784df394bd1f5e02583e9ba437437
Patrick Shannon <patrickshannon05021993@gmail.com>
2018-02-07 15:40:08 -0500

    Removes the old testing method from insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -25,29 +25,3 @@ function insert(value, position, array){
 }
 
 
-//// Tests!!!
-let testArray = [1,7,4,8,9,0,3]
-let newArray = []
-function resetTestArrays(){
-	newArray = testArray.slice(0)
-}
-// insert
-newArray = insert(6,1,testArray)
-console.assert(String(newArray) === String([1,6,7,4,8,9,0,3]),`6 doesn't correctly get inserted into index 1 of 'testArray' [${newArray}]`)
-console.log(`6 correctly inserted into 'testArray'!`)
-resetTestArrays()
-newArray = insert(2,6,testArray)
-console.assert(String(newArray) === String([1,7,4,8,9,0,2,3]),`2 doesn't correctly get inserted into index 6 of 'testArray' [${newArray}]`)
-console.log(`2 correctly inserted into 'testArray'!`)
-resetTestArrays()
-newArray = insert(9,7,testArray)
-console.assert(String(newArray) === String([1,7,4,8,9,0,3,9]),`9 doesn't correctly get inserted into index 7 of 'testArray' [${newArray}]`)
-console.log(`9 correctly inserted into 'testArray'!`)
-resetTestArrays()
-newArray = insert(100,-9,testArray)
-console.assert(!newArray,`Ludacris insertions aren't rejected, 'insert 100 into -9th index of test array.'`)
-console.log('rediculous insertions are rejected!')
-console.log('All insert tests complete')
-
-// sort
-
