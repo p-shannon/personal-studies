5ba8238dd8d4b65447ebd8ab8988c2eae002f265
Patrick Shannon <patrickshannon05021993@gmail.com>
2018-02-07 15:35:41 -0500

    Removed pointless console logs, removed the tests, I know they work for now but they may return. Exported the TestSeries and finally adds semicolons to the code. This is a very bad commit, and I should work on not doing this as often in the future.

--- function-tester.js
+++ function-tester.js
@@ -1,8 +1,8 @@
 //TODO make TestSeries act more like instances of a group of tests able to be used in another's tests.
 
 function TestSeries() {
-	this.tests = []
-	this.results = []
+	this.tests = [];
+	this.results = [];
 }
 
 TestSeries.prototype.addTest = function(operation, assertion, assertionDescription, ...inputs){
@@ -14,34 +14,25 @@ TestSeries.prototype.addTest = function(operation, assertion, assertionDescripti
 	})
 }
 
+//TODO Make use of promise objects to implement a runtime count
 TestSeries.prototype.run = function(){
-	let resultString = "=====[ BEGIN TESTS ]===== \n \n"
-	let passes = 0
+	let resultString = "=====[ BEGIN TESTS ]===== \n \n";
+	let passes = 0;
 	for (let i = 0; i < this.tests.length; i++){
-		console.log('ding')
 		if(this.tests[i].operation(...this.tests[i].inputs)===this.tests[i].assertion){
-			this.results[i] = true
-			resultString += `..PASS..: ${this.tests[i].assertionDescription} \n`
-			++passes
+			this.results[i] = true;
+			resultString += `..PASS..: ${this.tests[i].assertionDescription} \n`;
+			++passes;
 		}
 		else{
-			this.results[i] = false
-			resultString += `!!FAIL!!: ${this.tests[i].assertionDescription} \n`
+			this.results[i] = false;
+			resultString += `!!FAIL!!: ${this.tests[i].assertionDescription} \n`;
 		}
 	}
-	resultString += "\n=====[   RESULTS   ]===== \n \n"
-	let percent = (passes/this.tests.length)*100
-	resultString += `${percent}% of tests passed. \n \n=====[  END TESTS  ]=====`
-	return resultString
+	resultString += "\n=====[   RESULTS   ]===== \n \n";
+	let percent = (passes/this.tests.length)*100;
+	resultString += `${percent}% of tests passed. \n \n=====[  END TESTS  ]=====`;
+	return resultString;
 }
-////TESTS
-//actually work
-function sum(a,b=a){
-	console.log("a",a,"b",b,"sum",a+b)
-	return a+b
-}
-let meta = new TestSeries()
-meta.addTest(sum, 3,"Adds 1 and 2 to get 3.", 1, 2)
-meta.addTest(sum, 9,"Adds two numbers to get 9.", 8,100)
-//console.log(meta.tests)
-console.log(meta.run())
+
+module.exports = TestSeries;
