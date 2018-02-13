fe320b74c9f5bd40ede33eb680b9902cae17d392
Patrick Shannon <patrickshannon05021993@gmail.com>
2018-02-12 23:46:34 -0500

    adds semicolons to the insert function.

--- insertionSort.js
+++ insertionSort.js
@@ -3,25 +3,25 @@
 ////////////////////////////////////////////////////////////
 //NOTE: This thing loops over an array twice, gunna have to cut down on that.
 function insert(value, position, array){
-	let tempArray = array.slice(0)
+	let tempArray = array.slice(0);
 	if (position === tempArray.length){
-		tempArray[position] = value
-		return tempArray
+		tempArray[position] = value;
+		return tempArray;
 	}
 	if (position < 0 || position > tempArray.length){
-		return false
+		return false;
 	}
 	for (let i = array.length-1; i >= 0; i--){
 		if (i != position){
-			tempArray[i+1] = tempArray[i]
+			tempArray[i+1] = tempArray[i];
 		}
 		else{
-			tempArray[i+1] = tempArray[i]
-			tempArray[i] = value
-			break
+			tempArray[i+1] = tempArray[i];
+			tempArray[i] = value;
+			break;
 		}
 	}
-	return tempArray
+	return tempArray;
 }
 
 function findSmallest(startRange, array){
