a781d093a0b5242b9d7ca6e1825040cd0f184432
Patrick Shannon <patrickshannon05021993@gmail.com>
2018-02-12 23:21:23 -0500

    Adds some function tests for insertionsort.js (borked)

--- TEST-insertionSort.js
+++ TEST-insertionSort.js
@@ -12,3 +12,9 @@ searchTest.addTest(iSort.findSmallest, 5, "Properly finds 2 in the subarray", 3,
 searchTest.addTest(iSort.findSmallest, 1, "Properly finds 1 in the subarray", 1, [0,1,1,7,4,2,9]);
 
 console.log(searchTest.run());
+
+let sortTest = new TestSeries();
+sortTest.addTest(iSort.sort, [1,2,3,4,5], "Properly sorts the damn array.", [5,2,3,1,4]);
+sortTest.addTest(iSort.sort, [1,1,2,2,3,4,5], "Properly sorts the damn array.", [5,2,1,3,2,1,4]);
+
+console.log(sortTest.run());
